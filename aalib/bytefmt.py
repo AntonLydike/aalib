65e9923a276de62e2e9c3e941097fd67e91cd22c
Anton Lydike <me@antonlydike.de>
2025-08-22 18:13:56 +0100

    add bytefmt

---
+++ aalib/bytefmt.py
@@ -0,0 +1,26 @@
+
+_suffix = ['','k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y', 'R', 'Q']
+
+def bytefmt(num: int, decimal = True):
+    base = 1024
+    if decimal:
+        base = 1000
+    i = 0
+    while num > base:
+        num /= base
+        i += 1
+    # whole numbers:
+    if num % 1 < 0.01:
+        num = str(int(num))
+    elif num < 10:
+        num = format(num, '.2f')
+    elif num < 100:
+        num = format(num, '.1f')
+    else:
+        num = str(int(num))
+    
+    if i == 0:
+        return f"{num}B"
+    if not decimal:
+        return f"{num}{_suffix[i].upper()}iB"
+    return f"{num}{_suffix[i]}B"
